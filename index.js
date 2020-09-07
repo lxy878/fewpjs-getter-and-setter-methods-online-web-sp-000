e121d405cd8be50ca15f866e8caf8d784907a321
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 08:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ class Circle{
   constructor(radius){
     this.radius = radius;
   }
+  
   get diameter(){
     return this.radius * 2;
+  }
+
+  get circumference(){
+    return this.diameter * Math.PI
   }
 }
